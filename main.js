96fa055077b9236fd31c5f4a447577217d9d7e4d
Elisaul Velasquez <elisaulvz@gmail.com>
2024-03-24 15:33:27 -0300

    agregando funcionalidad al fomulario

---
+++ main.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.boton2'); 
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_pni3rhn';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
